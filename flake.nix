1d669dd19a53e7612a52cc35fb7c14792192cccb
tupakkatapa <jesse@ponkila.com>
2024-01-03 07:03:34 +0200

    module

--- flake.nix
+++ flake.nix
@@ -7,15 +7,25 @@
     devenv.url = "github:cachix/devenv";
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} ({withSystem, ...}: rec {
       imports = [
         inputs.devenv.flakeModule
       ];
 
       systems = nixpkgs.lib.systems.flakeExposed;
 
-      perSystem = { self', pkgs, system, ... }: rec {
+      perSystem = {
+        self',
+        pkgs,
+        system,
+        ...
+      }: {
         # Nix code formatter, accessible through 'nix fmt'
         formatter = nixpkgs.legacyPackages.${system}.alejandra;
 
@@ -52,8 +62,22 @@
             yarnLock = ./yarn.lock;
             yarnNix = ./yarn.nix;
           };
-          default = packages.blog;
+          default = self'.packages.blog;
         };
       };
-    };
-  }
+
+      flake = let
+        inherit (self) outputs;
+      in {
+        # Overlay packages
+        overlays.default = final: prev:
+          withSystem prev.stdenv.hostPlatform.system ({self', ...}: self'.packages);
+
+        # NixOS modules
+        nixosModules.coditon-blog = {
+          imports = [./module.nix];
+          nixpkgs.overlays = [self.overlays.default];
+        };
+      };
+    });
+}
---
+++ module.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.coditon-blog;
+in {
+  options.services.coditon-blog = {
+    enable = lib.mkEnableOption "blog.coditon.com";
+
+    address = lib.mkOption {
+      type = lib.types.str;
+      default = "0.0.0.0";
+      description = "Host address for the service";
+    };
+
+    port = lib.mkOption {
+      type = lib.types.int;
+      default = 8080;
+      description = "Port number for the service";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd = {
+      services.coditon-blog = {
+        description = "blog.coditon.com";
+        after = ["network.target"];
+        wantedBy = ["multi-user.target"];
+        serviceConfig = {
+          Type = "simple";
+          Restart = "on-failure";
+          DynamicUser = true;
+          ExecStart = lib.concatStringsSep " \\\n\t" [
+            "${pkgs.coditon-blog}"
+            "--port ${toString cfg.port}"
+            "--address ${cfg.address}"
+          ];
+        };
+      };
+    };
+  };
+}
+
